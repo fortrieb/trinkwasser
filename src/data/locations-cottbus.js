6b0b3c1b4b40303796ed6016cad9403177f2b3a3
Benji <dev@fortrieb.org>
2020-03-11 08:19:06 +0100

    Add data for Cottbus

---
+++ src/data/locations-cottbus.js
@@ -0,0 +1,3 @@
+tw.data.locations = 
+{"Neuhausen":{},"Byhleguhre-Kaupen, Straupitz-Buschm\u00fchle, Horst":{}, "Byhleguhre":{}, "Cottbuser Fehrower Weg":{}, "Cottbus Sachsendorf":{}, "Eichow":{}, "Kathlow-Sergen":{}}
+;
---
+++ src/data/zones-cottbus.js
@@ -0,0 +1,168 @@
+tw.data.zones = {
+    "Neuhausen": {
+      "magnesium": 6.1,
+      "sulfat": 10,
+      "natrium": 5,
+      "chlorid": 5,
+      "calcium": 34,
+      "kalium": 0.9,
+      "nitrat": 0.4,
+      "hardness": 6,
+      "description": "Neuhausen"
+    },
+    "Byhleguhre-Kaupen, Straupitz-Buschm\u00fchle, Horst": {
+      "magnesium": 15.5,
+      "sulfat": 206,
+      "natrium": 34,
+      "chlorid": 46,
+      "calcium": 106,
+      "kalium": 4.4,
+      "nitrat": 4.3,
+      "hardness": 18,
+      "description": "Neuhausen"
+    },
+    "Byhleguhre": {
+      "magnesium": 2.1,
+      "sulfat": 52,
+      "natrium": 6,
+      "chlorid": 6,
+      "calcium": 45,
+      "kalium": 0.6,
+      "nitrat": 0.2,
+      "hardness": 6,
+      "description": "Neuhausen"
+    },
+    "Cottbuser Fehrower Weg": {
+      "magnesium": 9.1,
+      "sulfat": 122,
+      "natrium": 16,
+      "chlorid": 34,
+      "calcium": 88,
+      "kalium": 2.4,
+      "nitrat": 2.2,
+      "hardness": 15,
+      "description": "Neuhausen"
+    },
+    "Cottbus Sachsendorf": {
+      "magnesium": 11.4,
+      "sulfat": 98,
+      "natrium": 16,
+      "chlorid": 27,
+      "calcium": 90,
+      "kalium": 2.2,
+      "nitrat": 1.2,
+      "hardness": 11,
+      "description": "Neuhausen"
+    },
+    "Eichow": {
+      "magnesium": 6.2,
+      "sulfat": 70,
+      "natrium": 7,
+      "chlorid": 21,
+      "calcium": 70,
+      "kalium": 1.2,
+      "nitrat": 0.6,
+      "hardness": 11,
+      "description": "Neuhausen"
+    },
+    "Kathlow-Sergen": {
+      "magnesium": 3.7,
+      "sulfat": 50,
+      "natrium": 8,
+      "chlorid": 14,
+      "calcium": 59,
+      "kalium": 4.2,
+      "nitrat": 1.0,
+      "hardness": 9,
+      "description": "Neuhausen"
+    },
+    "Gablenz": {
+      "magnesium": 7.1,
+      "sulfat": 65,
+      "natrium": 5,
+      "chlorid": 6,
+      "calcium": 34,
+      "kalium": 1.1,
+      "nitrat": 0.1,
+      "hardness": 6,
+      "description": "Neuhausen"
+    },
+    "Tagebau J\u00e4nschwalde": {
+      "magnesium": 7.4,
+      "sulfat": 104,
+      "natrium": 18,
+      "chlorid": 26,
+      "calcium": 101,
+      "kalium": 2.5,
+      "nitrat": 3.6,
+      "hardness": 16,
+      "description": "Neuhausen"
+    },
+    "Sacrow-Waldow": {
+      "magnesium": 3.2,
+      "sulfat": 18,
+      "natrium": 7,
+      "chlorid": 9,
+      "calcium": 51,
+      "kalium": 0.8,
+      "nitrat": 2.0,
+      "hardness": 8,
+      "description": "Neuhausen"
+    },
+    "Gro\u00df D\u00f6bbern": {
+      "magnesium": 14.3,
+      "sulfat": 100,
+      "natrium": 7,
+      "chlorid": 5,
+      "calcium": 38,
+      "kalium": 1.9,
+      "nitrat": 0.3,
+      "hardness": 9,
+      "description": "Neuhausen"
+    },
+    "Drebkau": {
+      "magnesium": 9.7,
+      "sulfat": 88,
+      "natrium": 9,
+      "chlorid": 17,
+      "calcium": 60,
+      "kalium": 3.5,
+      "nitrat": 1.3,
+      "hardness": 11,
+      "description": "Neuhausen"
+    },
+    "Brodtkowitz-Wiesendorf": {
+      "magnesium": 8.7,
+      "sulfat": 80,
+      "natrium": 9,
+      "chlorid": 17,
+      "calcium": 60,
+      "kalium": 1.5,
+      "nitrat": 0.8,
+      "hardness": 10,
+      "description": "Neuhausen"
+    },
+    "Halbe": {
+      "magnesium": 4.6,
+      "sulfat": 0.7,
+      "natrium": 16,
+      "chlorid": 10,
+      "calcium": 52,
+      "kalium": 1.1,
+      "nitrat": 0.6,
+      "hardness": 8,
+      "description": "Neuhausen"
+    },
+    "Teupitz": {
+      "magnesium": 3.0,
+      "sulfat": 2.1,
+      "natrium": 10,
+      "chlorid": 10,
+      "calcium": 33,
+      "kalium": 0.8,
+      "nitrat": 0.4,
+      "hardness": 5,
+      "description": "Neuhausen"
+    }
+};
+tw.data.averageValues = { "natrium": 8.8, "kalium": 1.6, "calcium": 77.7, "magnesium": 17.9, "chlorid": 18.2, "nitrat": 14.9, "sulfat": 51.5, "hardness": 14.9 };
